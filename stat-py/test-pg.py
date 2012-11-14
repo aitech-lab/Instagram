31fbcdd794c0aed14dccc897d6c64425e8a912c3
peko <peko@gasubasu.com>
2012-11-14 14:09:45 +0400

    postgres

--- stat-py/test-pg.py
+++ stat-py/test-pg.py
@@ -1,5 +1,5 @@
 import psycopg2
-import time
+import time, sys
 import httplib
 import simplejson as json
 import ConfigParser,  StringIO
@@ -21,17 +21,19 @@ DB_HOST = cfg.get("config","DB_HOST")
 DB_NAME = cfg.get("config","DB_NAME")
 DB_USER = cfg.get("config","DB_USER")
 DB_PASS = cfg.get("config","DB_PASSWORD")
+DB_PORT = cfg.get("config","DB_PORT", 5432)
 
 try:
-    conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (DB_NAME, DB_USER, DB_HOST, DB_PASS))
+    pgcon = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s' port='%s'" % (DB_NAME, DB_USER, DB_HOST, DB_PASS, DB_PORT))
 except:
     print "I am unable to connect to the database"
+    sys.exit()
 
-cur = conn.cursor()
+pgcur = pgcon.cursor()
 
 def grabPiceOfInstagram(lat, lng, client_id) :
     t         = round(time.time()/60)*60
-    distance  = "5000"
+    distance  = "50000"
     step      = 60*6
     c=0
     while (c<288) : # 144 = 1 day step 10 min 
@@ -49,16 +51,17 @@ def grabPiceOfInstagram(lat, lng, client_id) :
         print time.ctime(min_timestamp), ">>>", time.ctime(max_timestamp)
         
         if resp.status == 200 :
+            print "https://api.instagram.com%s" % url
             d = json.loads(resp.read())
             for i in d['data']: 
                 print time.ctime(int(i['created_time'])), i['id'], i['location']['latitude'], i['location']['longitude']
-                
-                print(
-                    str(i['location']['latitude'  ])+", "+
-                    str(i['location']['longitude'] )+", "+
-                    str(int(i['created_time'])     )+", "+
-                    str(i['id']                    )+"\n"
-                )
+                #print i
+                SQL = "INSERT INTO insta(location,json,unixtime) VALUES(point(%s,%s),%s,%s)"
+                DATA = (i['location']['latitude'],i['location']['longitude'], str(i), i['created_time'],)
+                print SQL % DATA
+                pgcur.execute(SQL, DATA)
+            if len(d['data']) :
+                pgcon.commit()
             c+=1
         else :
             print "https://api.instagram.com%s" % url
@@ -66,4 +69,5 @@ def grabPiceOfInstagram(lat, lng, client_id) :
 
 
 id1 = "6f3a4f4174774f5da4380a481299a995"
-grabPiceOfInstagram("55.7173666", "37.5535744",id1 ) 
+#https://maps.google.com/maps?hl=en&ll=55.747792,37.630234&spn=0.104926,0.338173&t=m&z=12
+grabPiceOfInstagram("55.747792", "37.630234",id1 ) 
